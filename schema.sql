13d95377d23071970215d426e147e005b12e1594
danishsharma496 <danishsharma496@gmail.com>
2023-04-28 11:50:57 +0530

    updating jobs in job_listing table

---
+++ schema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100) UNIQUE NOT NULL,
+    joined DATE DEFAULT CURRENT_DATE
+);
+CREATE TABLE login (
+  id SERIAL PRIMARY KEY,
+  hash VARCHAR(100) NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
+);
+ 
+ 
+ 
+CREATE TABLE job_listing (
+  id SERIAL PRIMARY KEY,
+  job_title VARCHAR(100) NOT NULL,
+  company_name VARCHAR(100) NOT NULL,
+  location VARCHAR(100) NOT NULL,
+  job_description TEXT NOT NULL,
+  deadline DATE NOT NULL,
+  posted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  is_active BOOLEAN DEFAULT TRUE,
+  contact_phone VARCHAR(20) NOT NULL,
+  contact_email VARCHAR(100) NOT NULL
+);
+
+ 
+CREATE TABLE interested_jobs (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  job_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (job_id) REFERENCES job_listing (id)
+);
+
---
+++ server.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const app = express();
+const bcrypt=require("bcrypt-nodejs");
+const cors = require('cors');
+const knex = require('knex'); 
+const { response } = require('express');
+
+const postgres = knex({
+    client: 'pg',
+    connection: {
+        host : '127.0.0.1', //localhost
+        user : 'adonis', //add your user name for the database here
+        // port: 3001, // add your port number here
+        password : '', //add your correct password in here/ we dont have so keep it empty don't fill qrong one
+        database : 'smart-brain' //add your database name you created here
+    }
+  });
+
+  
+
+ 
+
+app.use(express.json());
+app.use(cors());
+ 
+
+
+ 
+
+app.get('/',(req,res)=>{
+
+    postgres.select('*').from('users').then(data=>{
+        res.json(data);
+    })
+    .catch(err =>res.status(400).json("Unable to get data "));
+    
+
+})
+app.get('/profile/:id',(req,res)=>{
+    const {id}=req.params ; 
+
+    postgres.select('*').from('users').where('id' , id).then(data=>{
+        if(data.length){
+            res.json(data[0]);
+        }
+        else {
+            res.status(400).json("User not found ");
+        }
+      
+    })
+    .catch(err =>res.status(400).json("Unable to get data "));
+
+
+})
+
+app.post('/signin',(req, res)=>{
+
+ 
+    postgres.select('email' , 'hash').from('login')
+        .where('email',req.body.email)
+        .then(data=>{
+        const isvalid =  bcrypt.compareSync(req.body.password , data[0].hash);
+        console.log(isvalid);
+             if(isvalid){
+ 
+                return postgres.select('*').from('users')
+                .where('email','=', req.body.email)
+                .then(user=>{
+                    console.log(user);
+                    res.json(user[0]);
+                })
+                .catch(err =>res.status(400).json("Unable to get user"));
+            
+             }
+             res.status(400).json("Wrong Credentials  ");
+        })
+        .catch(err =>res.status(400).json("Wrong "));
+   
+ 
+})
+
+app.post("/register",(req,res)=>{
+    const {email , name , password}=req.body;
+ 
+    const hash = bcrypt.hashSync(password );
+ 
+ 
+    postgres.transaction(trx=>{
+        trx.insert({
+            hash:hash , 
+            email:email 
+        })
+        .into('login')
+        .returning('email')
+        .then( loginEmail=>{
+                postgres('users')
+                .returning('*')
+                .insert({
+                email:loginEmail[0].email,
+                name:name,
+                joined:new Date()
+            })
+            .then(users=>{
+                res.json(users[0]);
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
+    .catch(err =>res.status(400).json("Unable to register "));
+   
+
+})
+
+app.post('/job_listing', (req, res) => {
+    const { job_title, company_name, location, job_description, contact_phone, contact_email, deadline } = req.body;
+    
+    postgres('job_listing')
+      .insert({ job_title, company_name, location, job_description, contact_phone, contact_email, deadline })
+      .returning('*')
+      .then(data => res.json(data[0]))
+      .catch(err => res.status(400).json('Unable to create job listing'));
+  });
+  
+app.listen(3001, ()=>{
+    console.log("server is working ");
+}); 
